89db00bca850c1ecd1dbff69010526a90ad0266b
Romulo <romulogomes13@hotmail.com>
2018-11-07 16:02:46 -0300

    add form

---
+++ src/components/lancamentos/Form.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface FormLancamentosProps {
+}
+
+export default class FormLancamentos extends React.Component<FormLancamentosProps, any> {
+  public render() {
+    return (
+      <div>
+        Form Aluno
+      </div>
+    );
+  }
+}
