5cef3dcd28ea28cf95346d1546749b6257a77a76
Romulo <romulogomes13@hotmail.com>
2018-11-08 09:58:19 -0300

    add Form contas

---
+++ src/components/contas/Form.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface FormContasProps {
+}
+
+export default class FormContas extends React.Component<FormContasProps, any> {
+  public render() {
+    return (
+      <div>
+        FOrm contas
+      </div>
+    );
+  }
+}
