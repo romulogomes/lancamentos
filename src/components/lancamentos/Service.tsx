dea8079535d3e63f09bb6f575921e0791bbdc218
Romulo <romulogomes13@hotmail.com>
2018-11-08 11:58:59 -0300

    add tipo retorno service

--- src/components/lancamentos/Service.tsx
+++ src/components/lancamentos/Service.tsx
@@ -1,23 +1,24 @@
 import axios, { AxiosPromise } from 'axios';
 import { API_URL } from '../../ApiUrl'
+import { Lancamento } from './List';
 
 class LancamentoService {
 
     url_lancamentos = API_URL+'lancamentos/';
 
-    listaLancamentos() : AxiosPromise{
+    listaLancamentos() : AxiosPromise<Lancamento[]>{
         return axios.get(this.url_lancamentos);
     }
     
-    getInfosLancamento(idLancamento : number): AxiosPromise{
+    getInfosLancamento(idLancamento : number): AxiosPromise<Lancamento>{
         return axios.get(this.url_lancamentos + idLancamento);
     }
     
-    gravaLancamento(data: any): AxiosPromise{
+    gravaLancamento(data: any): AxiosPromise<Lancamento[]>{
         return axios.post( this.url_lancamentos, data);
     }
     
-    editarLancamento(data : any): AxiosPromise{
+    editarLancamento(data : any): AxiosPromise<Lancamento>{
         return axios.put(this.url_lancamentos, data);
     }
     
